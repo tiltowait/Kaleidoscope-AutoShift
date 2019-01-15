fdebf48eb5028a671960d37b8f24242cfc9e23b1
tiltowait <tiltowaitt+github@icloud.com>
2019-01-14 16:49:46 -0800

    Misc

--- src/Kaleidoscope/AutoShift.h
+++ src/Kaleidoscope/AutoShift.h
@@ -1,6 +1,6 @@
 // -*- mode: c++ -*-
-// Kaleidoscope-AutoShift - 
-// Copyright (C) 2019 
+// Kaleidoscope-AutoShift - Shift a key if it's been held for some time.
+// Copyright (C) 2019 Jared Lindsay 
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
